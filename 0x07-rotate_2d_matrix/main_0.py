eb671e7d93a6a11df22f37b937886864bc9d6acd
userlog404 <101346110+userlog404@users.noreply.github.com>
2023-03-14 12:15:09 +0300

    Update main_0.py

--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 """
-Test 0x16 - Rotate 2D Matrix
+Test 0x07 - Rotate 2D Matrix
 """
-
-
-def print_matrix(matrix):
-    """ Print matrix with new lines """
-    print("=====================")
-    for i in range(len(matrix)):
-        print(matrix[i])
-
-
 rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
 
 if __name__ == "__main__":
@@ -19,32 +10,4 @@ if __name__ == "__main__":
               [7, 8, 9]]
 
     rotate_2d_matrix(matrix)
-    print_matrix(matrix)
-
-    matrix = [[1, 2],
-              [4, 5]]
-
-    rotate_2d_matrix(matrix)
-    print_matrix(matrix)
-
-    matrix = [[1]]
-
-    rotate_2d_matrix(matrix)
-    print_matrix(matrix)
-
-    matrix = [[1, 2, 3, 8],
-              [4, 5, 6, 0],
-              [7, 8, 9, 2],
-              [4, 3, 2, 1]]
-
-    rotate_2d_matrix(matrix)
-    print_matrix(matrix)
-
-    matrix = [[1, 2, 3, 8, 9],
-              [4, 5, 6, 0, 7],
-              [7, 8, 9, 2, 6],
-              [4, 3, 2, 1, 5],
-              [-2, 8, 0, -8, 4]]
-
-    rotate_2d_matrix(matrix)
-    print_matrix(matrix)
+    print(matrix)
