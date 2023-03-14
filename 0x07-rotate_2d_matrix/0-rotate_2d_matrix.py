714c7732a6a58f085349cfd2977ac966d0e77cb4
userlog404 <101346110+userlog404@users.noreply.github.com>
2023-03-14 12:15:30 +0300

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
-""" Rotate 2D Matrix 90 Degrees Clockwise"""
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise."""
 
 
 def rotate_2d_matrix(matrix):
-    """ Function for rotating 2D Matrix 90 degrees clockwise
-    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
-    - Do not return anything. The matrix must be edited in-place.
-    - You can assume the matrix will have 2 dimensions and will not be empty.
-    """
-
-    n = len(matrix[0])
-
-    for i in range(n - 1, -1, -1):
-        for j in range(0, n):
-            matrix[j].append(matrix[i].pop(0))
+    """Rotate a 2D matrix 90 degrees clockwise."""
+    ziped = zip(*reversed(matrix))
+    for i, j in enumerate(ziped):
+        matrix[i] = list(j)
